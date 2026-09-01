af1ceedc826dd7ce01e4f15a53957dcb325ab606
Adarshco <adarshsinghal98@gmail.com>
2026-09-01 14:32:38 +0530

    Update job.route.js

--- routes/job.route.js
+++ routes/job.route.js
@@ -1,13 +1,23 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controllers/job.controller.js";
+
+import {
+    getAdminJobs,
+    getAllJobs,
+    getJobById,
+    postJob
+} from "../controllers/job.controller.js";
 
 const router = express.Router();
 
+// Recruiter must be logged in to post a job
 router.route("/post").post(isAuthenticated, postJob);
-router.route("/get").get(isAuthenticated, getAllJobs);
+
+// Public routes
+router.route("/get").get(getAllJobs);
+router.route("/get/:id").get(getJobById);
+
+// Recruiter must be logged in
 router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
-router.route("/get/:id").get(isAuthenticated, getJobById);
 
 export default router;
-
